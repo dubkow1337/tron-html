432a832d3dbbc716b128de909f1b01df8043edc8
REDRABBIT <dubkow14@gmail.com>
2026-06-15 10:40:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-const CELL_SIZE = 20;
+const CELL_SIZE = 16;
 const WIDTH = canvas.width / CELL_SIZE;
 const HEIGHT = canvas.height / CELL_SIZE;
 let MOVE_INTERVAL = 70;
